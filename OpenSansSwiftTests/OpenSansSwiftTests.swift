2d919292f887e31a8939db9df9077ef51d9c63a1
Hemanta Sapkota <laex.pearl@gmail.com>
2015-02-22 09:40:53 +1100

    Add tests

--- OpenSansSwiftTests/OpenSansSwiftTests.swift
+++ OpenSansSwiftTests/OpenSansSwiftTests.swift
@@ -8,29 +8,55 @@
 
 import UIKit
 import XCTest
+import OpenSansSwift
 
 class OpenSansSwiftTests: XCTestCase {
     
+    let openSansSwift = OpenSans()
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testFontRegistration() {
+        let registration = OpenSans.registerFonts()
+        XCTAssertTrue(registration, "Registration should be successful")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
+    func testLoadOpenSansFonts() {
+        var font = UIFont.openSansFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-Regular loaded sucessfully.")
+        
+        font = UIFont.openSansBoldFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-Bold loaded sucessfully.")
+        
+        font = UIFont.openSansBoldItalicFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-BoldItalic loaded sucessfully.")
+        
+        font = UIFont.openSansExtraBoldFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-Extrabold loaded sucessfully.")
+        
+        font = UIFont.openSansExtraBoldItalicFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-ExtraboldItalic loaded sucessfully.")
+        
+        font = UIFont.openSansItalicFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-Italic loaded sucessfully.")
+        
+        font = UIFont.openSansLightFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-Light loaded sucessfully.")
+        
+        font = UIFont.openSansLightItalicFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-LightItalic loaded sucessfully.")
+        
+        font = UIFont.openSansSemiboldFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-Semibold loaded sucessfully.")
+        
+        font = UIFont.openSansSemiboldItalicFontOfSize(10)
+        XCTAssertNotNil(font, "OpenSans-SemiboldItalic loaded sucessfully.")
     }
     
 }
